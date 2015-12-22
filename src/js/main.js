c06a5460f99efa4129bfd860dbb5fdefa36ba27f
benhassara <bhassara@gmail.com>
2015-12-22 14:59:12 -0500

    add day countdown, needs to be styled

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  var weddingDay = new Date('July 10, 2016');
+  var today = new Date();
+  var daysLeft = numDays(weddingDay.getTime() - today.getTime());
+
+  $('#counter').html(daysLeft);
+
+  setTimeout(function() {
+      daysLeft = numDays(weddingDay.getTime() - today.getTime());
+      $('#counter').html(daysLeft);
+  }, 60000);
 });
+
+/** takes in milliseconds, spits out number of days */
+function numDays(ms) {
+    var approxDays = ms / 1000 / 60 / 60 / 24;
+    return Math.ceil(approxDays);
+}
